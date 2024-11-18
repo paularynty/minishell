aea2f18f7766b90cece69c416d48d8a924497a2b
paularynty <paula.rynty@gmail.com>
2024-11-18 12:15:29 +0200

    test commit for paula's branch

--- srcs/signals/signals.c
+++ srcs/signals/signals.c
@@ -6,12 +6,13 @@
 /*   By: prynty <prynty@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 15:09:39 by prynty            #+#    #+#             */
-/*   Updated: 2024/11/18 11:48:45 by prynty           ###   ########.fr       */
+/*   Updated: 2024/11/18 12:14:52 by prynty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+//this doesn't work, program segfaults with ctrl+d
 static void	signal_ctrl_d(int signal)
 {	
 	if (signal == SIGQUIT)
