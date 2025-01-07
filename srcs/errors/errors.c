99194bb8805a09e2012a875affe0fa7ec0e535d7
paularynty <paula.rynty@gmail.com>
2025-01-07 11:29:40 +0200

    test commit, this branch should work 115/146 test cases and not get stuck in infinite loop

--- srcs/errors/errors.c
+++ srcs/errors/errors.c
@@ -45,41 +45,6 @@ void	error_cmd(t_mini *shell, char *cmd, char *error_str, int ex)
 
 }
 
-// //check for '/' character at end of directory, if yes, ex = 126, if not, ex = 1
-// void	error_cmd(t_mini *shell, char *cmd, char *error_str, int ex)
-// {
-// 	ft_putstr_fd("minishell: ", 2);
-// 	ft_putstr_fd(cmd, 2);
-// 	write(2, ": ", 2);
-// 	if (!error_str)
-// 		error_str = strerror(errno);
-// 	if (error_str && error_str[0] != '\0')
-// 		ft_putendl_fd(error_str, 2);
-// 	shell->exit_code = ex;
-// 	exit(shell->exit_code);
-// }
-
-// //check for '/' character at end of directory, if yes, ex = 126, if not, ex = 1
-// void	error_cmd(t_mini *shell, char *cmd)
-// {
-// 	int	exitcode;
-
-// 	exitcode = errno;
-// 	ft_putstr_fd("minishell: ", 2);
-// 	if (errno == EACCES || errno == ENOENT || errno == EISDIR)
-// 		perror(cmd);
-// 	else
-// 	{
-// 		ft_putstr_fd(cmd, 2);
-// 		ft_putendl_fd(": command not found", 2);
-// 	}
-// 	if (exitcode == EACCES || exitcode == EISDIR)
-// 		shell->exit_code = 126;
-// 	else
-// 		shell->exit_code = 127;
-// 	exit(shell->exit_code);
-// }
-
 void	error_file(t_mini *shell, char *file, char *error_str, int ex)
 {
 	char	buffer[1024];
